c813f3ed221338215c50b68247901950b195672c
okshi <johnstarhafiz@gmail.com>
2022-02-07 21:18:47 +0600

    Setup folder stracture and initial route

---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+     return (
+          <div>
+               
+          </div>
+     );
+};
+
+export default ErrorPage;
