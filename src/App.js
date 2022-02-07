e09328eab6634f633cce4d36b3cc6d51a05445ef
okshi <johnstarhafiz@gmail.com>
2022-02-07 23:05:37 +0600

    Setup folder and routing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
