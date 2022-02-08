a73a979d5c8bc3d29fee473a63c9bfd1c0e81338
Dev70 <Shanto9585>
2022-02-08 22:41:47 +0600

    banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <section className="hero-section">
+            <div className="container">
+                <div className="row pt-5 pb-5">
+
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Banner;
