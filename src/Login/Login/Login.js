1a48532b239e2d0b24b80b43388855c4aaabbd49
mehedi-hasan-saikat <m.h.saikat15@gmail.com>
2022-02-09 20:10:51 +0600

    update All

--- src/Login/Login/Login.js
+++ src/Login/Login/Login.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { useForm } from "react-hook-form";
+// import React from 'react';
+// import { Col, Container, Row } from 'react-bootstrap';
+// import { useForm } from "react-hook-form";
 
-import './Login.css'
+// import './Login.css'
 
 
 
-const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    return (
-        <div className='py-5'>
-            <Container>
-                <Row>
-                    <Col md={{ span: 8, offset: 2 }}>
+// const Login = () => {
+//     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+//     const onSubmit = data => console.log(data);
+//     return (
+//         <div className='py-5'>
+//             <Container>
+//                 <Row>
+//                     <Col md={{ span: 8, offset: 2 }}>
 
-                        <div className="login-form text-center">
-                            <h2 className='mb-5'>Login to Edu Bro</h2>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <input className='w-75 mb-3'  {...register("email", { required: true })} placeholder='Enter Email' /> <br />
-                                <input className='w-75 mb-3' {...register("password", { required: true })} placeholder='Enter Password' /> <br />
+//                         <div className="login-form text-center">
+//                             <h2 className='mb-5'>Login to Edu Bro</h2>
+//                             <form onSubmit={handleSubmit(onSubmit)}>
+//                                 <input className='w-75 mb-3'  {...register("email", { required: true })} placeholder='Enter Email' /> <br />
+//                                 <input className='w-75 mb-3' {...register("password", { required: true })} placeholder='Enter Password' /> <br />
 
-                                <button type='submit'>Login</button>
+//                                 <button type='submit'>Login</button>
 
-                        <div className="login-box mx-auto">
-                            <h2 className='text-center'>Login</h2>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <input className='w-75' {...register("email", { required: true })} placeholder='Enter Email' /> <br />
-                                <input className='w-75' {...register("password", { required: true })} placeholder='Enter Password' /> <br />
-                                <input type="submit" />
+//                         <div className="login-box mx-auto">
+//                             <h2 className='text-center'>Login</h2>
+//                             <form onSubmit={handleSubmit(onSubmit)}>
+//                                 <input className='w-75' {...register("email", { required: true })} placeholder='Enter Email' /> <br />
+//                                 <input className='w-75' {...register("password", { required: true })} placeholder='Enter Password' /> <br />
+//                                 <input type="submit" />
 
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-};
+//                             </form>
+//                         </div>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </div>
+//     );
+// };
 
-export default Login;
+// export default Login;
