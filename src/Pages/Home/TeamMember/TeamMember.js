b05f81fbb70a83c2a675a780d8eae0bdf11feb2b
okshi <johnstarhafiz@gmail.com>
2022-02-10 13:30:59 +0600

    Done team meamber some editing

--- src/Pages/Home/TeamMember/TeamMember.js
+++ src/Pages/Home/TeamMember/TeamMember.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Saikat from '../../../Assets/Images/TeamMember/Saikat.png'
 import Yeasin from '../../../Assets/Images/TeamMember/Yeasin.png'
+import Hafiz from '../../../Assets/Images/TeamMember/jrHafiz.png'
 import './TeamMember.css'
 const TeamMember = () => {
     return (
@@ -29,9 +30,9 @@ const TeamMember = () => {
                 </div>
                 <div class="col">
                     <div class="card h-100 hover">
-                        <img src={Saikat} class="card-img-top img-fluid img-fluid" alt="..." />
+                        <img src={Hafiz} class="card-img-top img-fluid img-fluid" alt="..." />
                         <div class="card-body">
-                            <h5 class="card-title">PETER PARKER</h5>
+                            <h5 class="card-title">Okshi Hafiz</h5>
                             <p class="card-text">Tour Expert</p>
                         </div>
 
