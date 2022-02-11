416ce5fa9694e96e7660033f0b28308458f41590
mehedi-hasan-saikat <m.h.saikat15@gmail.com>
2022-02-12 01:33:12 +0600

    add Contact Section and Update Route v

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -17,11 +17,11 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto menu">
-                            <Nav.Link href="#home" className='menu-item'>Home</Nav.Link>
+                            <Nav.Link href="#home" as={NavLink} to="/home"  className='menu-item'>Home</Nav.Link>
                             <Nav.Link href="#find" className='menu-item'>Find Questions</Nav.Link>
                             <Nav.Link href="#forum" className='menu-item'>Forums</Nav.Link>
                             <Nav.Link href="#ask" className='menu-item'>Ask Questions</Nav.Link>
-                            <Nav.Link href="#contact" className='menu-item'>Contact</Nav.Link>
+                            <Nav.Link as={NavLink} to="/contact"  className='menu-item'>Contact</Nav.Link>
                             <p className="text-white me-3 ms-2 text-center mt-2">{user.displayName}</p>
                             { !user.email ?<Nav.Link    as={NavLink} to="/login" className='menu-item'>
                             Login
