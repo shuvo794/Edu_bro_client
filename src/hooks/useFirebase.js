c109179c06975feb257296f8365edf0c492888d0
Dev70 <Shanto9585>
2022-02-14 18:38:01 +0600

    user to data base

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -7,7 +7,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
-  
+
 } from "firebase/auth";
 
 import { useEffect, useState } from "react";
@@ -20,7 +20,7 @@ const useFirebase = () => {
   const [user, setUser] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
-  
+
 
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
@@ -28,59 +28,62 @@ const useFirebase = () => {
 
   //REGISTER WITH EMAIL END PASSWORD
 
-  const registerUser = ( email, password, name,location, navigate) =>{
+  const registerUser = (email, password, name, location, navigate) => {
+    sendUserInfoToDb(email)
     setIsLoading(true)
     createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
 
-    const destination = location?.state?.from || '/'
-    navigate(destination)
-    setError("");
-    
-    
-    const newUser = {email, displayName:name}
-     //console.log(newUser);
-    setUser(newUser)
+      .then((userCredential) => {
 
-    //SEND NAME IN THE FIREBASE
-    updateProfile(auth.currentUser, {
-     displayName:name
-   }).then(() => {
-     
-   }).catch((error) => {
-     
-   });
-   
-  })
-  .catch((error) => {
-    setError(error.message);
-  })
-  .finally(() => setIsLoading(false));
+        const destination = location?.state?.from || '/'
+        navigate(destination)
+        setError("");
+
+
+        const newUser = { email, displayName: name }
+        //console.log(newUser);
+        setUser(newUser)
+
+        //SEND NAME IN THE FIREBASE
+        updateProfile(auth.currentUser, {
+          displayName: name
+        }).then(() => {
+
+        }).catch((error) => {
+
+        });
+
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+
   }
 
   //lOGIN WITW EMAIL AND PASSWORD COUSTM 
-  const loginWithOwnEmaiAndPass  = (email, password, location, navigate) =>{
-   
+  const loginWithOwnEmaiAndPass = (email, password, location, navigate) => {
     setIsLoading(true)
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const destination = location?.state?.from || '/'
-      navigate(destination)
-    setError('');
-  })
-  .catch((error) => {
-    setError(error.message);
-  })
-  .finally(() => setIsLoading(false));
+      .then((userCredential) => {
+        const destination = location?.state?.from || '/'
+        navigate(destination)
+        setError('');
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+
   }
 
   // GOOGLE PROVIDER LOGIN
   const loginWithGoogle = (location, navigate) => {
+
     setIsLoading(true)
     signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
-       
         const destination = location?.state?.from || '/'
         navigate(destination)
         setError("");
@@ -89,26 +92,37 @@ const useFirebase = () => {
         setError(error.message);
       })
       .finally(() => setIsLoading(false));
+
   };
 
   //LOG OUT USER METHOD
-  const userLogOut = () =>{
+  const userLogOut = () => {
     setIsLoading(true)
     signOut(auth)
-    .then(() => {
-      
-    }).catch((error) => {
-      setError(error.message)
-    })
-    .finally(() => setIsLoading(false));
+      .then(() => {
+
+      }).catch((error) => {
+        setError(error.message)
+      })
+      .finally(() => setIsLoading(false));
   }
+  const sendUserInfoToDb = (email) => {
+    fetch("http://localhost:5000/addUserInfo", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ email }),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+  };
+
 
   //OBSERVER USER STATE
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        
+
       } else {
         setUser({});
       }
