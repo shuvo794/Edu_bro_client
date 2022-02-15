bd3c7a89eaaa9b73cc1a600a056b72c8be778abc
Dev70 <Shanto9585>
2022-02-15 23:43:03 +0600

    update

--- src/Pages/AllQuestions/QuestionCart.js
+++ src/Pages/AllQuestions/QuestionCart.js
@@ -14,14 +14,7 @@ const QuestionCart = ({ data }) => {
             <div className="card custom-cart h-100 hover">
                 <iframe title="question" src={viewUrl}
                     className="img-fluid rounded-start w-100 " style={{ height: "500px" }} allow="autoplay"></iframe>
-                <div className="card-body">
-                    <h5 className="card-title"><b>Subject Name:</b> {subject}</h5>
-                    <h5 className="card-title"><b>Subject Code:</b> {code}</h5>
-                    <h5 className="card-title"><b>Semester:</b> {semester}</h5>
-                    <h5 className="card-text "><b>Year:</b> {year}</h5>
-                    <div className="btn btn-success me-5 " ><a href={download} className="ahref">Download</a></div>
-                    <div className="btn btn-danger ms-5" ><a href={viewUrl} className="ahref">Show</a></div>
-                </div>
+
             </div>
         </div>
     );
