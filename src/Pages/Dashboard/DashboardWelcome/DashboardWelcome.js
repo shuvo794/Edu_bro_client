5aabb030314fdb9065183c0e1ea1286c0dcda29c
monibBormon <monibbormon14@gmail.com>
2022-02-16 22:46:43 +0600

    user profile update on dashboard

--- src/Pages/Dashboard/DashboardWelcome/DashboardWelcome.js
+++ src/Pages/Dashboard/DashboardWelcome/DashboardWelcome.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import useFirebase from '../../../hooks/useFirebase';
 
 const DashboardWelcome = () => {
     const { user } = useFirebase()
+    console.log(user);
     return (
         <div className='dashboard-welcome'>
-            <h3>Hello, <span>{user.displayName}</span></h3>
-            <p className='font-lg'>Welcome to Edu Bro.</p>
+            <Row>
+                <Col md={6}>
+                    <h2>Hello, <span className='fs-1'>{user.displayName}</span></h2>
+                    <p className='fs-4'>Welcome to Edu Bro.</p>
+                </Col>
+                <Col md={6}>
+                    <div className="dashboard-image">
+                        <img src={user.displayName ? user.photoURL : 'https://i.ibb.co/Xsnkx3L/user.png'} alt="user" />
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
