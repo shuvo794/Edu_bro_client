c222337d6236546a208c8844ef94b6a52b320fc6
monibBormon <monibbormon14@gmail.com>
2022-02-17 18:23:00 +0600

    import question component

--- src/Pages/FindQuestions/FindQuestions.js
+++ src/Pages/FindQuestions/FindQuestions.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './FindQuestions.css';
+import { FaSearch } from 'react-icons/fa';
+import Questions from '../Home/Questions/Questions'
 
 const FindQuestions = () => {
     return (
@@ -12,12 +14,13 @@ const FindQuestions = () => {
                             <h2>Search Questions</h2>
                             <div className="search-form">
                                 <input type="text" placeholder='Search Questions' />
-                                <button className='search-btn'>Search</button>
+                                <button className='search-btn'>Search <FaSearch /></button>
                             </div>
                         </div>
                     </Col>
                 </Row>
             </Container>
+            <Questions />
         </div>
     );
 };
