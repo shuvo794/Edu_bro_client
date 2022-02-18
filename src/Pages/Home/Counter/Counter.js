7d338d6ca880970577580ce1fea210bee57b4736
monibBormon <monibbormon14@gmail.com>
2022-02-18 11:16:01 +0600

    homepage counter section done

---
+++ src/Pages/Home/Counter/Counter.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaBookmark, FaUserAlt, FaGlobeAsia, FaCommentDots } from 'react-icons/fa';
+import './Counter.css';
+
+const Counter = () => {
+    return (
+        <div className='counter-area'>
+            <Container>
+                <Row>
+                    <Col md={3}>
+                        <div className="single-counter text-center shadow py-5">
+                            <div className="counter-icon">
+                                <FaBookmark className='icon' />
+                            </div>
+                            <div className="counter-text">
+                                <h3>170</h3>
+                                <p>Total Question</p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={3}>
+                        <div className="single-counter text-center shadow py-5">
+                            <div className="counter-icon">
+                                <FaUserAlt className='icon' />
+                            </div>
+                            <div className="counter-text">
+                                <h3>539</h3>
+                                <p>Users</p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={3}>
+                        <div className="single-counter text-center shadow py-5">
+                            <div className="counter-icon">
+                                <FaGlobeAsia className='icon' />
+                            </div>
+                            <div className="counter-text">
+                                <h3>40</h3>
+                                <p>Countries</p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={3}>
+                        <div className="single-counter text-center shadow py-5">
+                            <div className="counter-icon">
+                                <FaCommentDots className='icon' />
+                            </div>
+                            <div className="counter-text">
+                                <h3>60</h3>
+                                <p>Question Asked</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Counter;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import AllReviews from '../AllReviews/AllReviews';
 import Banner from '../Banner/Banner';
-
 import TeamMember from '../TeamMember/TeamMember';
-
 import Questions from '../Questions/Questions';
 import Blogs from '../Blogs/Blogs';
+import Counter from '../Counter/Counter';
 
 
 const Home = () => {
@@ -16,8 +15,12 @@ const Home = () => {
                <Questions></Questions>
                <AllReviews></AllReviews>
                <Blogs></Blogs>
+               <Counter />
+
+
+
                {/* <TeamMember></TeamMember> */}
-               
+
           </div>
      );
 };
