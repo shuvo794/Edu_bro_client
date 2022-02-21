3b010022b83fc675d383212590f6fd5a06217442
shuvo794 <shuvohossain794@gmail.com>
2022-02-21 23:59:40 +0600

    adde

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AllReviews from '../AllReviews/AllReviews';
 import Banner from '../Banner/Banner';
-import TeamMember from '../TeamMember/TeamMember';
+
 import Questions from '../Questions/Questions';
 import Blogs from '../Blogs/Blogs';
 import Counter from '../Counter/Counter';
+// import Questions from './../Questions/Questions';
 
 
 const Home = () => {
