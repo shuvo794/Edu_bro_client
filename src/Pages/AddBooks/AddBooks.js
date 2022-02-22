a7aedf9aa6e242e2e313677d3e067111c5bdba48
hasnatulalam <hasnatualam056@gmail.com>
2022-02-22 22:41:08 +0600

    final

--- src/Pages/AddBooks/AddBooks.js
+++ src/Pages/AddBooks/AddBooks.js
@@ -20,23 +20,23 @@ const AddBooks = () => {
                     <Col md={{ span: 8, offset: 2 }}>
                         <div className="login-form text-center">
                             <h2 className='mb-5'>Add Your Books</h2>
-                            <form
-                                
+                            <form>
+
                                 <input className='w-75 mb-3'  {...register("department", { required: true })}   {...(errors.title && <span>Name is required</span>)} placeholder='Enter the Department' /> <br />
-                            <input className='w-75 mb-3'  {...register("subject", { required: true })} placeholder='Enter Book Title' /> <br />
-                            <input className='w-75 mb-3'  {...register("code", { required: true })} placeholder='Enter Course Code' /> <br />
-                            <input className='w-75 mb-3' {...register("semester", { required: true })} placeholder='Enter Semester' /> <br />
-                            <input className='w-75 mb-3' {...register("year", { required: true })} placeholder='Question Year' /> <br />
+                                <input className='w-75 mb-3'  {...register("subject", { required: true })} placeholder='Enter Book Title' /> <br />
+                                <input className='w-75 mb-3'  {...register("code", { required: true })} placeholder='Enter Course Code' /> <br />
+                                <input className='w-75 mb-3' {...register("semester", { required: true })} placeholder='Enter Semester' /> <br />
+                                <input className='w-75 mb-3' {...register("year", { required: true })} placeholder='Question Year' /> <br />
 
-                            <input className='w-75 mb-3' {...register("ypdfLinkear", { required: true })} placeholder='Question Link' /> <br />
+                                <input className='w-75 mb-3' {...register("ypdfLinkear", { required: true })} placeholder='Question Link' /> <br />
 
-                            <button type='submit'>Submit</button>
-                        </form>
+                                <button type='submit'>Submit</button>
+                            </form>
 
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
         </div >
     );
