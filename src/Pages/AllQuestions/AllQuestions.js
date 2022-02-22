1434cbda5a5a23560b1b8347b096b6f3f93f3b49
Dev70 <Shanto9585>
2022-02-22 19:04:46 +0600

    server update

--- src/Pages/AllQuestions/AllQuestions.js
+++ src/Pages/AllQuestions/AllQuestions.js
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react';
 import QuestionCart from './QuestionCart';
 
 const AllQuestions = () => {
-    const [data, setData] = useState([]);
+    const [questions, setQuestions] = useState([]);
     useEffect(() => {
-        fetch('/Test.json')
+        fetch('http://localhost:5000/allQuestions')
             .then(res => res.json())
-            .then(data => setData(data))
+            .then(data => setQuestions(data))
     }, [])
+
+
     return (
         <div className="container text-black mt-5 mb-5" >
             <div className="d-flex my-5 justify-content-center"><h1 className="user-desire-question">Desire Questions</h1></div>
             <div className="row row-cols-1 row-cols-md-3 g-4">
-                {data?.map((data) => (
+                {questions?.map((question) => (
                     <QuestionCart
-                        key={data.id}
-                        data={data}>
+                        key={question.id}
+                        data={questions}>
 
                     </QuestionCart>
                 ))}
--- src/Pages/AskQuestions/AskQuestions.js
+++ src/Pages/AskQuestions/AskQuestions.js
@@ -3,37 +3,55 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+
+const onSubmit = data => {
+
+    fetch(`http://localhost:5000/postQuestion`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+    })
+        .then((res) => res.json())
+        .then((result) => {
+            console.log(result)
+
+            alert('Question Added')
+        });
+};
+
+
 const AskQuestions = () => {
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     return (
         <div className='py-5'>
-           
 
 
-      
+
+
             <Container>
                 <Row>
                     <Col md={{ span: 8, offset: 2 }}>
                         <div className="login-form text-center">
                             <h2 className='mb-5'>Add Your Question</h2>
-                            <form >
-                            <input className='w-75 mb-3'  {...register("department", { required: true })} placeholder='Enter the Department' /> <br />
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input className='w-75 mb-3'  {...register("department", { required: true })} placeholder='Enter the Department' /> <br />
                                 <input className='w-75 mb-3'  {...register("subject", { required: true })} placeholder='Enter Course Title' /> <br />
                                 <input className='w-75 mb-3'  {...register("code", { required: true })} placeholder='Enter Course Code' /> <br />
                                 <input className='w-75 mb-3' {...register("semester", { required: true })} placeholder='Enter Semester' /> <br />
                                 <input className='w-75 mb-3' {...register("year", { required: true })} placeholder='Question Year' /> <br />
 
-                                <input className='w-75 mb-3' {...register("ypdfLinkear", { required: true })} placeholder='Question Link' /> <br />
+                                <input className='w-75 mb-3' {...register("driveLink", { required: true })} placeholder='Question Link' /> <br />
 
                                 <button type='submit'>Submit</button>
                             </form>
-                           
+
                         </div>
                     </Col>
                 </Row>
             </Container>
-       
+
         </div>
     );
 };
