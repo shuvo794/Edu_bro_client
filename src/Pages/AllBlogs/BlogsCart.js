4c8b9c638adc18050b1419910502a0c5e04bf2cb
Dev70 <Shanto9585>
2022-03-02 02:26:05 +0600

    question update

--- src/Pages/AllBlogs/BlogsCart.js
+++ src/Pages/AllBlogs/BlogsCart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BlogsCart = ({data}) => {
     
-    const { topic, blog,yserName, email } = data
+    const { topic, blog,userName, email } = data
     // let googleId = driveLink?.slice(32, 65);
     // console.log(driveLink)
 
@@ -13,7 +13,7 @@ const BlogsCart = ({data}) => {
         <div className="card custom-cart h-100 hover">
             <div className="card-body">
                 <h5 className="card-title"><b>Book Name:</b> {topic}</h5>
-                <h5 className="card-title"><b>Writer Name:</b> {yserName}</h5>
+                <h5 className="card-title"><b>Writer Name:</b> {userName}</h5>
                 <h5 className="card-title"><b>Email:</b> {email}</h5>
              <p> {blog}</p>
                 {/* <div className="d-flex justify-content-around pt-5" >
