d1387797132ea7f2c8be1d7be37f8e29beae4a11
Dev70 <Shanto9585>
2022-03-04 02:48:32 +0600

    update

--- src/Pages/Dashboard/MyBlogs/MyBlogs.js
+++ src/Pages/Dashboard/MyBlogs/MyBlogs.js
@@ -1,58 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { FaTrashAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../../hooks/useFirebase';
 
 
 
 const MyBlogs = () => {
+    const { user } = useFirebase()
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch(` http://localhost:5000/myBlogs/${user?.email}`)
+            .then((res) => res.json())
+            .then((data) => setBlogs(data));
+    }, [user?.email]);
+
+console.log(blogs)
+
+
+
+// const handleBlogDeleteRequest = (data) => {
+//     fetch("http://localhost:5000/requestBlogsDelete", {
+//         method: "PUT",
+//         headers: { "content-type": "application/json" },
+//         body: JSON.stringify(data),
+//     })
+//         .then((res) => res.json())
+//         .then((result) => console.log(result));
+//     alert('Added admin successfully!!!!')
+
+// };
+
+
+
+
     return (
-    //     < div className="container all-order-container" >
-    //     <div className="text-center pb-3">
-    //         <h1 className="mb-5 text-center pt-5">Total Blogs <span className="text-danger">{orders.length}</span>  </h1>
-    //     </div>
+    <div className='my-questions'>
+        <div className='d-flex justify-content-between align-items-center my-question-header'>
+            <h2>My Blogs</h2>
+            <Link to={'/dashboard/add-blogs'}>
+                <button className='add-btn btn-danger'>Add Blogs</button>
+            </Link>
+        </div>
+        <table className="table table-dark" style={{ width: "100%" }}>
+                <thead  >
+                    <tr className="bg-dark text-white mb-3 p-2" style={{ bblog: "1px solid red" }}>
 
-    //     <table className="table table-dark" style={{ width: "100%" }}>
-    //         <thead  >
-    //             <tr className="bg-dark text-white mb-3 p-2" style={{ border: "1px solid red" }}>
+                        <th >Number</th>
+                        <th >Topic</th>
+            
+                        <th >Image</th>
+                       
+                        <th >Status</th>
+                        <th >Request To Delete</th>
+                    
+                    </tr>
+                </thead>
+                {blogs?.map((blog, index) => (
+                    <tbody key={blog._id}>
+                        <tr role="row" style={{ blog: "2px solid gray" }} >
+                            <th scope="row">{index + 1}</th>
+                            <td>{blog.topic}</td>
+                            <td><img style={{ width: "70px", height: "50px" }} src={blog.BlogImg} alt="" /></td>
+                            <td>{blog.status}</td>
+                            <td> <button
+                                className="btn btn-danger"
+                                // onClick={() => handleBlogDeleteRequest(blog._id)}
+                            >
+                                Delete Blog
+                            </button></td>
+                        </tr>
+                    </tbody>
 
-    //                 <th >Number</th>
-    //                 <th >Place</th>
-    //                 <th >Location</th>
-    //                 <th >Image</th>
-    //                 <th >Price</th>
-    //                 <th >Transportation</th>
-    //                 <th >Status</th>
-    //                 <th >Update</th>
-    //             </tr>
-    //         </thead>
-    //         {orders?.map((order, index) => (
-    //             <tbody key={order._id}>
-    //                 <tr role="row" style={{ border: "2px solid gray" }} >
-    //                     <th scope="row">{index + 1}</th>
-    //                     <td>{order.place}</td>
-    //                     <td>{order.location}</td>
-    //                     <td><img style={{ width: "70px", height: "50px" }} src={order.img} alt="" /></td>
-    //                     <td>{order.price}</td>
-    //                     <td>{order.transportation}</td>
-    //                     <td>
-    //                         <div >
-    //                             <select onChange={handleSelectValue} className="pending p-2 ">
-    //                                 <option defaultValue={order.status}>{order.status}</option>
-    //                                 <option defaultValue="approved">Approved</option>
-    //                                 <option defaultValue="pending">Pending</option>
-    //                                 <option defaultValue="cancelled">Cancelled</option>
-    //                             </select>
-    //                         </div>
-    //                     </td>
-        
-    //                 </tr>
-    //             </tbody>
-
-    //         ))}
-    //     </table>
-    // </div >
-
+                ))}
+            </table>
+    </div>
     );
 };
 
