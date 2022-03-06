2bf9b647dbb061b3692c891cd529ff859c416990
Dev70 <Shanto9585>
2022-03-06 18:37:01 +0600

    update

--- src/Pages/Dashboard/AddBooksDashboard/AddBooksDashboard.js
+++ src/Pages/Dashboard/AddBooksDashboard/AddBooksDashboard.js
@@ -26,24 +26,24 @@ const AddBooksDashboard = () => {
     };
     return (
         <Container>
-        <Row>
-            <Col md={{ span: 8, offset: 2 }}>
-                <div className="login-form text-center">
-                    <h2 className='mb-5'>Add Books</h2>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input className='w-75 mb-3'  {...register("bookName", { required: true })} placeholder='Book Name' /> <br />
-                        <input className='w-75 mb-3'  {...register("author", { required: true })} placeholder='Author Name' /> <br />
-                        <input className='w-75 mb-3'  {...register("subject", { required: true })} placeholder='Subject Name' /> <br />
-                        <input className='w-75 mb-3' {...register("edition", { required: true })} placeholder='Book Edition' /> <br />
-                        <input className='w-75 mb-3' {...register("year", { required: true })} placeholder='Year' /> <br />
-                        <input className='w-75 mb-3' {...register("driveLink", { required: true })} placeholder='Question Link' /> <br />
-                        <button type='submit'>Submit</button>
-                    </form>
+            <Row>
+                <Col md={{ span: 8, offset: 2 }}>
+                    <div className="login-form text-center">
+                        <h2 className='mb-5'>Add Books</h2>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input className='w-75 mb-3'  {...register("bookName", { required: true })} placeholder='Book Name' /> <br />
+                            <input className='w-75 mb-3'  {...register("author", { required: true })} placeholder='Author Name' /> <br />
+                            <input className='w-75 mb-3'  {...register("subject", { required: true })} placeholder='Subject Name' /> <br />
+                            <input className='w-75 mb-3' {...register("edition", { required: true })} placeholder='Book Edition' /> <br />
+                            <input className='w-75 mb-3' {...register("year", { required: true })} placeholder='Year' /> <br />
+                            <input className='w-75 mb-3' {...register("driveLink", { required: true })} placeholder='Question Link' /> <br />
+                            <button type='submit'>Submit</button>
+                        </form>
 
-                </div>
-            </Col>
-        </Row>
-    </Container>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
