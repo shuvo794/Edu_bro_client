c8a3d6302ec2288ebd210d935033cd34fdf49cb2
Dev70 <Shanto9585>
2022-03-10 21:12:16 +0600

    update

---
+++ src/Pages/AdminDashboard/AdminQuestion/AdminQuestion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminQuestion = () => {
+    return (
+        <div>
+            <h1>questions</h1>
+        </div>
+    );
+};
+
+export default AdminQuestion;
