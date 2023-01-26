4e1347fd9c0c841287cf248853c2f9d9517ac0d9
shuvo794 <shuvohossain794@gmail.com>
2023-01-26 11:02:56 +0600

    update

--- src/Pages/AllSyllabus/AllSyllabus.js
+++ src/Pages/AllSyllabus/AllSyllabus.js
@@ -4,51 +4,38 @@ import { getSyllabusAction } from '../../ReduxManagement/Redux/actions';
 import SyllabusCart from './SyllabusCart';
 
 const AllSyllabus = () => {
+  const dispatch = useDispatch();
 
-    // const [syllabus, setSyllabus] = useState([]);
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/')
-    //         .then(res => res.json())
-    //         .then(data => setSyllabus(data))
-    // }, [])
+  const allSyllabusData = useSelector(
+    (state) => state.questionsArchiveData.syllabusData
+  );
 
-    // Start data store via Redux toolkit 
+  useEffect(() => {
+    dispatch(getSyllabusAction());
+  }, []);
 
-    const dispatch = useDispatch()
+  // end data store via Redux toolkit
 
-    const allSyllabusData = useSelector((state) => state.questionsArchiveData.syllabusData)
-
-    useEffect(() => {
-        dispatch(getSyllabusAction())
-    }, [])
-
-    // end data store via Redux toolkit 
-
-
-    return (
-        <div className="container text-black mt-5 mb-5" >
-            <div className="d-flex my-5 justify-content-center"><h1 className="user-desire-question">Syllabus Collection</h1></div>
-            {
-                allSyllabusData.length === 0 ? <div className='text-center'>
-                    <div class="spinner-border m-5" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                </div> :
-                    <div className="row g-4">
-                        {allSyllabusData?.map((syllabuss) => (
-
-                            <SyllabusCart
-                                key={syllabuss.id}
-                                data={syllabuss}>
-
-                            </SyllabusCart>
-
-                        ))}
-                    </div>
-            }
-
+  return (
+    <div className="container text-black mt-5 mb-5">
+      <div className="d-flex my-5 justify-content-center">
+        <h1 className="user-desire-question">Syllabus Collection</h1>
+      </div>
+      {allSyllabusData.length === 0 ? (
+        <div className="text-center">
+          <div class="spinner-border m-5" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
         </div>
-    );
+      ) : (
+        <div className="row g-4">
+          {allSyllabusData?.map((syllabuss) => (
+            <SyllabusCart key={syllabuss.id} data={syllabuss}></SyllabusCart>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AllSyllabus;
